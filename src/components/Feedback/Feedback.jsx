aff1e682ce74aba879b62aeb24194072a642acee
olga <odubasss@gmail.com>
2025-03-30 20:56:02 +0200

    add changes

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,16 @@
+import styles from "./Feedback.module.css";
+
+const Feedback = ({ feedback, totalFeedback, positiveFeedback }) => {
+  return (
+    <div className={styles.feedback}>
+      <h2>Statistics</h2>
+      <p>Good: {feedback.good}</p>
+      <p>Neutral: {feedback.neutral}</p>
+      <p>Bad: {feedback.bad}</p>
+      <p>Total Feedback: {totalFeedback}</p>
+      <p>Positive Feedback: {positiveFeedback}%</p>
+    </div>
+  );
+};
+
+export default Feedback;
